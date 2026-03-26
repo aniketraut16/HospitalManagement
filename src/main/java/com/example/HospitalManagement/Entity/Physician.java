0b08c7930cf3dd934573631ee9effe8c002dd430
Nikhil Gupta <nikhilgupta932003@gmail.com>
2026-03-26 10:47:23 +0530

    refactor: Replace explicit getters, setters, and constructors in Physician entity with Lombok annotations

--- src/main/java/com/example/HospitalManagement/Entity/Physician.java
+++ src/main/java/com/example/HospitalManagement/Entity/Physician.java
@@ -6,7 +6,9 @@ import lombok.*;
 import java.util.List;
 
 @Entity
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "physician")
 public class Physician {
 
@@ -23,100 +25,7 @@ public class Physician {
     @Column(nullable = false, unique = true)
     private int ssn;
 
-    public int getEmployeeId() {
-        return employeeId;
-    }
+   
 
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public int getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(int ssn) {
-        this.ssn = ssn;
-    }
-
-    public Physician(int employeeId, String name, String position, int ssn) {
-        this.employeeId = employeeId;
-        this.name = name;
-        this.position = position;
-        this.ssn = ssn;
-    }
-
-    public Physician() {
-    }
-
-    @Id
-    @Column(name = "EmployeeID")
-    private int employeeId;
-
-    @Column(nullable = false)
-    private String name;
-
-    @Column(nullable = false)
-    private String position;
-
-    @Column(nullable = false, unique = true)
-    private int ssn;
-
-    public int getEmployeeId() {
-        return employeeId;
-    }
-
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public int getSsn() {
-        return ssn;
-    }
-
-    public void setSsn(int ssn) {
-        this.ssn = ssn;
-    }
-
-    public Physician(int employeeId, String name, String position, int ssn) {
-        this.employeeId = employeeId;
-        this.name = name;
-        this.position = position;
-        this.ssn = ssn;
-    }
-
-    public Physician() {
-    }
+   
 }
