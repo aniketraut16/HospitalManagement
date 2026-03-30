c90edd143e6c01ed4fe37c653dbf2e09852a08c8
aniketraut16 <rautnaniket@gmail.com>
2026-03-30 11:14:56 +0530

    Remove unused NikhilController, NikhilService, NikhilDTO, and NurseDTO classes to clean up the codebase.

--- src/main/java/com/example/HospitalManagement/Controller/NikhilController.java
+++
@@ -1,5 +0,0 @@
-package com.example.HospitalManagement.Controller;
-
-public class NikhilController {
-    // added
-}
--- src/main/java/com/example/HospitalManagement/dto/NurseDTO.java
+++
@@ -1,8 +0,0 @@
-package com.example.HospitalManagement.dto;
-
-public class NurseDTO {
-    private String name;
-    private String position;
-    private boolean registered;
-    private String availability;
-}
